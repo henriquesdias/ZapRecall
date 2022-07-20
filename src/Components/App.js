03c0e0bbbaddb7b3bedf14ba57570028099d37b9
henriquesdias <henrique.dias1915@gmail.com>
2022-07-20 01:37:02 -0300

    when the user clicks on 'Iniciar Recall' the next screen is shown

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,21 @@
-import ScreenPrincipal from "./ScreenPrincipal";
-
+import React from "react";
+import Flashcards from "./ScreenFlashcards";
 export default function App(){
+    const [screen , setScreen] = React.useState(true);
     return (
-        <ScreenPrincipal />
+        <>
+            {screen ? (
+                <div className="entry-screen">
+                    <img src="./assets/images/logo.png" alt="zap recall" />
+                    <h1>ZapRecall</h1>
+                    <div onClick={() => setScreen(false)}>
+                        Iniciar Recall
+                    </div>
+                </div>
+            ) : (
+                <Flashcards />
+            )}
+        </>
+
     )
 };
---
+++ src/Components/ScreenFlashcards.js
@@ -0,0 +1,13 @@
+function Top(){
+    return (
+        <div className="top">
+            <img src="./assets/images/logo-pequeno.png" alt="zap recall" />
+            <span>ZapRecall</span>
+        </div>
+    );
+}
+export default function Flashcards(){
+    return (
+        <Top/>
+    );
+}

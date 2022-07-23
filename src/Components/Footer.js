dfb1d81210ac2a89075122b3174629033a818dff
henriquesdias <henrique.dias1915@gmail.com>
2022-07-23 01:22:25 -0300

    if none of flashcards were answers with 'i dont remember' status, the message of 'congratulations' is shown

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,8 +2,23 @@ function Icon({name}){
     return <ion-icon name={name}></ion-icon>;
 }
 export default function Footer({valueInicial, numberOfQuestions,iconsFooter}){
+    let textResult = "Você não esqueceu de nenhum flashcard!";
+    let emoticonResult = "./assets/images/party.png"
+    let text = "Parabéns!"
+    const failed = iconsFooter.indexOf("close-circle") !== -1;
     return (
         <footer>
+            {(valueInicial === numberOfQuestions) ? 
+            (
+                <>
+                    <div>
+                    <img src={emoticonResult} alt="emoji" />  
+                    <span>{text}</span>
+                    </div>
+                    <p>{textResult}</p>
+                </>
+            )
+            : ''}
             {valueInicial}/{numberOfQuestions} Concluídos
             <div className="icons">
                 {iconsFooter.map( (icon,index) => (<Icon name={icon} key={index} />))}
--- src/Components/ScreenFlashcards.js
+++ src/Components/ScreenFlashcards.js
@@ -18,22 +18,22 @@ const flashcards = [
         question:'Podemos colocar (...) dentro do JSX',
         answer: 'expressões'
     },
-    {
-        question: 'O ReactDOM nos ajuda ...',
-        answer: 'interagindo com o DOM para colocar componentes React na mesma'
-    },
-    {
-        question:'Usamos o npm para ...',
-        answer: ' gerenciar os pacotes necessários e suas dependências'
-    },
-    {
-        question: 'Usamos props para ...',
-        answer: ' passar diferentes informações para componentes'
-    },
-    {
-        question: 'Usamos estado (state) para ...',
-        answer: 'dizer para o React quais informações devem renderizar a tela novamente, quando atualizadas'
-    }
+    // {
+    //     question: 'O ReactDOM nos ajuda ...',
+    //     answer: 'interagindo com o DOM para colocar componentes React na mesma'
+    // },
+    // {
+    //     question:'Usamos o npm para ...',
+    //     answer: ' gerenciar os pacotes necessários e suas dependências'
+    // },
+    // {
+    //     question: 'Usamos props para ...',
+    //     answer: ' passar diferentes informações para componentes'
+    // },
+    // {
+    //     question: 'Usamos estado (state) para ...',
+    //     answer: 'dizer para o React quais informações devem renderizar a tela novamente, quando atualizadas'
+    // }
 ]
 function Top(){
     return (

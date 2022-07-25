25eb560c0b9874b9644edc51474a4103088544fd
henriquesdias <henrique.dias1915@gmail.com>
2022-07-25 02:01:23 -0300

    the array of deck was changed for a different file call 'Data'

--- src/Components/ScreenFlashcards.js
+++ src/Components/ScreenFlashcards.js
@@ -1,41 +1,7 @@
 import React from "react";
 import Flashcard from "./Flashcard";
 import Footer from "./Footer";
-const flashcards = [
-  {
-    question: "O que é JSX ?",
-    answer: "Uma extensão de linguagem do JavaScript",
-  },
-  {
-    question: "O React é ...",
-    answer: "uma biblioteca javaScript para construção de interfaces",
-  },
-  {
-    question: "Componentes devem iniciar com...",
-    answer: "letra maiúscula",
-  },
-  {
-    question: "Podemos colocar (...) dentro do JSX",
-    answer: "expressões",
-  },
-  {
-    question: "O ReactDOM nos ajuda ...",
-    answer: "interagindo com o DOM para colocar componentes React na mesma",
-  },
-  {
-    question: "Usamos o npm para ...",
-    answer: " gerenciar os pacotes necessários e suas dependências",
-  },
-  {
-    question: "Usamos props para ...",
-    answer: " passar diferentes informações para componentes",
-  },
-  {
-    question: "Usamos estado (state) para ...",
-    answer:
-      "dizer para o React quais informações devem renderizar a tela novamente, quando atualizadas",
-  },
-];
+import flashcards from "./Data.js"
 function Top() {
   return (
     <div className="top">
